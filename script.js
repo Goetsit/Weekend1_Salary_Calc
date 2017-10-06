ed87444619e0954686fe80a773d992dc913aa2cc
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-06 17:00:55 -0500

    added  event listen and appends

--- script.js
+++ script.js
@@ -4,11 +4,35 @@ $(document).ready(readyNow);
 
 function readyNow(){
   console.log('JQ READY');
-  $('#firstNameBtn').on('click', firstNameVal);
+  $('#submitBtn').on('click', submitBtnSelect);
+
 
 }
 
 
-function firstNameVal(){
-  console.log($('#firstNameInput').val());
+function submitBtnSelect(){
+
+  var $inputFieldValueFirst = $('#firstNameInput').val();
+  var $inputFieldValueLast = $('#lastNameInput').val();
+  var $inputFieldValueIdNum = $('#idNumInput').val();
+  var $inputFieldValueJobTitle = $('#jobTitleInput').val();
+  var $inputFieldValueAnnualSalary = $('#annualSalInput').val();
+  var $btn = $(this);
+
+  console.log($btn, $inputFieldValueFirst, $inputFieldValueLast, $inputFieldValueIdNum);
+  console.log($btn, $inputFieldValueJobTitle, $inputFieldValueAnnualSalary);
+
+  appendDom();
+
+  function appendDom(){
+
+    $('.userResults').append('<ul><li>First Name:'+' '+ $inputFieldValueFirst +'</li>');
+    $('.userResults').append('<ul><li>Last Name:'+' '+ $inputFieldValueLast +'</li>');
+    $('.userResults').append('<ul><li>ID Number:'+' '+ $inputFieldValueIdNum +'</li>');
+    $('.userResults').append('<ul><li>Job Title:'+' '+ $inputFieldValueJobTitle +'</li>');
+    $('.userResults').append('<ul><li>Annaul Salary'+' '+ $inputFieldValueAnnualSalary +'</li></ul><br>');
+
+
+  }
+
 }
