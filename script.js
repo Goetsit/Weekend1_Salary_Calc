6ec149e3290c0a0725535e95d83fda30e170c835
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-10-07 20:34:59 -0500

    added method toFixed(), totals now sum

--- script.js
+++ script.js
@@ -4,23 +4,22 @@ $(document).ready(readyNow);
 var employees = [];  //empty array
 
 function readyNow(){
-
+  
   console.log('JQ READY');
   $('#submitBtn').on('click', submitBtnSelect);
   $('#removeBtn').on('click',removeButton);
   $('#submitBtn').on('click', appendDom);
-
+  
 } //event listeners
 
 function submitBtnSelect(){
-
+  
   var $inputFieldValueFirst = $('#firstNameInput').val();
   var $inputFieldValueLast = $('#lastNameInput').val();
   var $inputFieldValueIdNum = $('#idNumInput').val();
   var $inputFieldValueJobTitle = $('#jobTitleInput').val();
   var $inputFieldValueAnnualSalary = parseInt($('#annualSalInput').val());
   //Set variables for all input fields
-
   var employee = new Employee
   ($inputFieldValueFirst
     ,$inputFieldValueLast
@@ -28,11 +27,12 @@ function submitBtnSelect(){
     ,$inputFieldValueJobTitle
     ,$inputFieldValueAnnualSalary);
     employees.push(employee); //push into empty array
-
+    
+    
     totals(); //run totals for monthly expenses
   }
-
-
+  
+  
   function Employee(firstNameIn,lastNameIn,idNumberIn,jobTitleIn,annualSalaryIn) {
     this.firstName = firstNameIn;
     this.lastName = lastNameIn;
@@ -40,42 +40,41 @@ function submitBtnSelect(){
     this.jobTitle = jobTitleIn;
     this.annualSalary = annualSalaryIn;
   }
-
-
+  
+  
   function totals() {
-
+    
     var yearly = 0;
     var monthly = 0;
     for(var i = 0; i < employees.length; i += 1){
       yearly += employees[i].annualSalary;
     }
     monthly = Math.floor(yearly / 12);
-
-    $('#total').append(monthly);
-
-    console.log(monthly);
-
+    
+    $('#total').text(monthly.toFixed());
+    
   }
-
+  
   function appendDom(){
     for(var i = 0; i < employees.length; i += 1) {
       $('#empTable').append(newRow(employees[i],i));
     } //loop through employees array to append table
   }//calls newRow function to add row
-
-
+  
+  
   function newRow(employees,i){
     var rowInsert = '<tr>';
     rowInsert += '<td>' + employees.firstName +'</td>';
     rowInsert += '<td>' + employees.lastName + '</td>';
     rowInsert += '<td>' + employees.idNumber + '</td>';
     rowInsert += '<td>' + employees.jobTitle + '</td>';
-    rowInsert += '<td>' + employees.annualSalary + '</td>';
+    rowInsert += '<td class = "yearlySal">' + employees.annualSalary + '</td>';
     rowInsert += '</tr>';
-    $row = $(rowInsert);
-    return $row;
+    $newRow = $(rowInsert);
+    return $newRow;
   } //append rows to existing #empTable
-
+  
   function removeButton(){
     $('tr:last').remove();
+    
   }  //buttons removes last list item
